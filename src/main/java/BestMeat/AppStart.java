38344b538270287aeba8f6495423a1d112e54e0e
songjihoons1210 <gnsthd0621@naver.com>
2025-08-26 20:22:03 +0900

    ㅅㄷㄱ

--- src/main/java/BestMeat/AppStart.java
+++ src/main/java/BestMeat/AppStart.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppStart {
     public static void main(String[] args) {
-        // test123123
+        // test123123 ㅅㄷㅅ
 
     }
 }
