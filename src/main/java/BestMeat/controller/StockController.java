5f270dd5abba91c1d0c829c3e5af3a32060aeff3
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-08-26 20:30:22 +0900

    docs[stock] : API 명세서 기반의 메소드 주석 추가

--- src/main/java/BestMeat/controller/StockController.java
+++ src/main/java/BestMeat/controller/StockController.java
@@ -11,5 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
     private final StockService stockService;
 
+    // [stock01] 재고등록 - addStock()
+    // 기능설명 : [ 정육점번호(세션), 가격, 제품번호(select) ]를 받아, Stock DB에 저장한다.
+    // method : POST, URL : /stock/add
+    // 매개변수 : StockDto
+    // 반환타입 : int
+
+    // [stock02] 재고수정 - updateStock()
+    // 기능설명 : [ 정육점번호(세션), 가격 ]을 입력받아, [ 가격, 등록일 ]을 수정한다.
+    // method : PUT, URL : /stock/update
+    // 매개변수 : int sprice
+    // 반환타입 : boolean -> 성공 : true / 실패 : false
+
+    // [stock03] 재고삭제 - deleteStock()
+    // 기능설명 : [ 정육점번호(세션) ]을 받아, 해당하는 회사가 등록한 재고라면, 삭제한다.
+    // method : DELETE, URL : /stock/delete
+    // 매개변수 : int cno
+    // 반환타입 : boolean -> 성공 : true / 실패 : false
 
 } // class end
