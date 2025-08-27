95552088106e6b89b9cc828224f4c28c320d2c10
msh-94 <alstjdgh2003@naver.com>
2025-08-27 09:32:17 +0900

    file 서비스 , 컨트롤러 추가 ReviewDto, productDto , CompanyDto dto 구성

---
+++ src/main/java/BestMeat/controller/FileController.java
@@ -0,0 +1,13 @@
+package BestMeat.controller;
+
+import BestMeat.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
+public class FileController {
+    private final FileService fileService;
+}
---
+++ src/main/java/BestMeat/service/FileService.java
@@ -0,0 +1,8 @@
+package BestMeat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+}
