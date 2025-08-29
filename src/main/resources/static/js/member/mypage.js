9ea356a07a78ad7ec0521bd32b0a07ea3e29f20d
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-08-29 13:18:21 +0900

    feat[js] : 회원정보 상세조회 프론트 구현 완료

--- src/main/resources/static/js/member/mypage.js
+++ src/main/resources/static/js/member/mypage.js
@@ -7,8 +7,15 @@ const getMember = async ( ) => {
     const option = { method : "GET" };
     const response = await fetch( "/member/get", option );
     const data = await response.json();         console.log( data );
+    // * ming URL 만들기
+    let mimgURL = `/upload/member/${data.mimg}`
+    if ( data.mimg == null ){
+        mimgURL = 'https://placehold.co/100x100';
+    } // if end
+
+
     // 2. where + print
-    const mimgBox = document.querySelector('.mimg');
+    document.querySelector('.mimg').innerHTML = `<img src="${mimgURL}">`;
     document.querySelector('.mid').innerHTML = `${data.mid}`;
     document.querySelector('.mname').innerHTML = `${data.mname}`;
     document.querySelector('.mphone').innerHTML = `${data.mphone}`;
